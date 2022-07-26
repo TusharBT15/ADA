4177baebb57797432e2c449c3c8c5af92a696224
TusharBT15 <84076430+TusharBT15@users.noreply.github.com>
2022-07-26 21:17:33 +0530

    Update quick_sort.c

--- quick_sort.c
+++ quick_sort.c
@@ -8,11 +8,14 @@ void swap(int *a, int *b) {
   *b = t;
 }
 
-int partition(int array[], int low, int high) {
+int partition(int array[], int low, int high)
+{
   int pivot = array[high];
   int i = (low - 1);
-  for (int j = low; j < high; j++) {
-    if (array[j] <= pivot) {
+  for (int j = low; j < high; j++) 
+  {
+    if (array[j] <= pivot) 
+    {
       i++;
       swap(&array[i], &array[j]);
     }
@@ -21,16 +24,20 @@ int partition(int array[], int low, int high) {
   return (i + 1);
 }
 
-void quickSort(int array[], int low, int high) {
-  if (low < high) {
+void quickSort(int array[], int low, int high)
+{
+  if (low < high)
+  {
     int pi = partition(array, low, high);
     quickSort(array, low, pi - 1);
     quickSort(array, pi + 1, high);
   }
 }
 
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; ++i) {
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; ++i)
+  {
     printf("%d  ", array[i]);
   }
   printf("\n");
